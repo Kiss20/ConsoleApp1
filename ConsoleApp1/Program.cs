e0dd95b757ab54be0fc3c2cb15375f95ff1cc1bb
test1 <tempo@DESKTOP-DEQU8V8>
2021-01-20 12:28:25 +0200

    ketto

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World t!");
+            Console.WriteLine("Hello World ttititit!");
         }
     }
 }
